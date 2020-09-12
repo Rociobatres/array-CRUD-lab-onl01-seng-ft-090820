7265376919efc032f0122f42e1a2e31c5d08056d
Rocio Batres <jonasgirl21@gmail.com>
2020-09-12 21:39:57 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  ["wow", "water nation", "earth nation", "arrays!"]
+  ["wow", "water nation", "am", "arrays!"]
 end
 
 def add_element_to_end_of_array(array, element)
