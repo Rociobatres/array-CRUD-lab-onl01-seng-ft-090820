2919b83cbb59a546145e40ff0f10f213c3e6aae5
Rocio Batres <jonasgirl21@gmail.com>
2020-09-12 21:38:59 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -9,7 +9,6 @@ end
 def add_element_to_end_of_array(array, element)
   create_an_array.push("arrays!")
 end
-add_element_to_end_of_array
 
 def add_element_to_start_of_array(array, element)
   create_an_array.unshift("wow")
